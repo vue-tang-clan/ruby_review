1b024a4cff651a07cdb23bc4008a9f4ee4065d21
Peter Jang <peter.x.jang@gmail.com>
2018-06-18 20:30:50 -0500

    Add class example

--- employee.rb
+++ employee.rb
@@ -20,3 +20,53 @@ employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: t
 
 puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
 puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
+
+
+# Instances of a class
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def active
+    @active
+  end
+
+  def active=(input_active_status)
+    @active = input_active_status
+  end
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee1.print_info
+
+employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee2.print_info
+
+employee1.give_annual_raise
+puts employee1.first_name
+puts employee1.last_name
+puts employee1.salary
+puts employee1.active
+
+employee1.active = false
+puts employee1.active
